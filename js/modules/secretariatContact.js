5badb0de85ccfba4fc3ba9495c08c5b23f9c3c16
Thea <raebaraquio@gmail.com>
2017-02-20 06:54:26 +0800

    Updated reporting templates Updated secretariat email

--- js/modules/secretariatContact.js
+++ js/modules/secretariatContact.js
@@ -4,7 +4,7 @@ angular.module('secretariatContactMod',[])
 		return {
 	        add_line1: "4th Floor, DOF Building, BSP Complex, ",
 	        add_line2: "Roxas Boulevard, Manila, Philippines",
-	        email: "info@ph-eiti.com",
+	        email: "info@ph-eiti.org",
 	        phone: "+632 525 0487"
 	    }
 	}
