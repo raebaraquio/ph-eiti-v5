723cf0aac797c64329f2cf2a502f213cf0870b13
Thea <raebaraquio@gmail.com>
2017-10-02 00:22:01 +0800

    Enabled GA before deployment Fixed bug in insert users query Fixed bug in MSG Meetings controller

--- About-PH-EITI/js/controllers/MSGMeetingsController.js
+++ About-PH-EITI/js/controllers/MSGMeetingsController.js
@@ -31,73 +31,73 @@ aboutApp.controller('MSGMeetingsController',['$scope','dataFactory','$routeParam
             }
             catch(e){}
 
-            console.log($routeParams)
-            console.log(window.location)
-            console.log(mtgId)
-            // return false
-
             $scope.getPromise = dataFactory.getMSGmeetings();
             $scope.getPromise.then(function(response){
-                var meetings = response.data.meetings;
-                var newMeetings = []
-                if (meetings.length > 0) {
-                    for (var i=meetings.length;i>=0;i--) {
-                        var mtg = meetings[i];
-                        var annexes = [];
-                        if (mtg) {
-                            if (mtg.with_annex=='1') {
-                                var temp_annexes = mtg.meeting_annexes;
-                                if (temp_annexes.match(/---/gi)) {
-                                    var a = temp_annexes.split('---');
-                                    for (var idx=0;idx<a.length;idx++) {
-                                        if (a[idx].match(/~~~/gi)) { // With File
-                                            var b = a[idx].split('~~~');
-                                            annexes.push({
-                                                annex_id : b[0],
-                                                title : b[1],
-                                                file : b[2]
-                                            });
+                if (response.data) {
+                    try {
+                        var meetings = response.data.meetings;
+                        var newMeetings = []
+                        if (meetings.length > 0) {
+                            for (var i=meetings.length;i>=0;i--) {
+                                var mtg = meetings[i];
+                                var annexes = [];
+                                if (mtg) {
+                                    if (mtg.with_annex=='1') {
+                                        var temp_annexes = mtg.meeting_annexes;
+                                        if (temp_annexes!=null) { 
+                                            if (temp_annexes.match(/---/gi)) {
+                                                var a = temp_annexes.split('---');
+                                                for (var idx=0;idx<a.length;idx++) {
+                                                    if (a[idx].match(/~~~/gi)) { // With File
+                                                        var b = a[idx].split('~~~');
+                                                        annexes.push({
+                                                            annex_id : b[0],
+                                                            title : b[1],
+                                                            file : b[2]
+                                                        });
+                                                    }
+                                                }
+                                            }
+                                            else { // One File
+                                                if (temp_annexes.match(/~~~/gi)) { // With File
+                                                    var b = temp_annexes.split('~~~');
+                                                    annexes.push({
+                                                        annex_id : b[0],
+                                                        title : b[1],
+                                                        file : b[2]
+                                                    });
+                                                }
+                                            }
                                         }
                                     }
+                                    meetings[i].annexes = annexes;
+                                    newMeetings.push(meetings[i]);
+
+                                    if (mtgId!==undefined && mtgId!==null) {
+                                        if (mtgId===mtg.mtgid) {
+                                            $scope.meeting_selected = meetings[i];
+                                            $scope.filterTitle = meetings[i].mtg_title;
+                                            $scope.selectedIdx = i;    
+                                        }
+                                    }    
                                 }
-                                else { // One File
-                                    if (temp_annexes.match(/~~~/gi)) { // With File
-                                        var b = temp_annexes.split('~~~');
-                                        annexes.push({
-                                            annex_id : b[0],
-                                            title : b[1],
-                                            file : b[2]
-                                        });
-                                    }
-                                }
+                                
                             }
-                            meetings[i].annexes = annexes;
-                            newMeetings.push(meetings[i]);
+                            
+                            $scope.meetings = newMeetings;
 
-                            if (mtgId!==null) {
-                                console.log(mtg)
-                                if (mtgId===mtg.mtgid) {
-                                    $scope.meeting_selected = meetings[i];
-                                    $scope.filterTitle = meetings[i].mtg_title;
-                                    $scope.selectedIdx = i;    
-                                }
-                            }    
+                            if (mtgId===undefined) {
+                                $scope.meeting_selected = newMeetings[0];
+                                $scope.filterTitle = newMeetings[0].mtg_title;
+                                $scope.selectedIdx = 0;    
+                            }
+                            
                         }
-                        
                     }
-                    
-                    $scope.meetings = newMeetings;
-
-                    if (mtgId===undefined) {
-                        $scope.meeting_selected = newMeetings[0];
-                        $scope.filterTitle = newMeetings[0].mtg_title;
-                        $scope.selectedIdx = 0;    
-                    }
-                    
+                    catch(mtgErr){}
                 }
                 delete $scope.getPromise;
             },function(error){
-                console.log(error)
                 delete $scope.getPromise;
             });
         }
--- rest/functions/insert-users.php
+++ rest/functions/insert-users.php
@@ -12,7 +12,7 @@ $query = "insert into users
             values
                 ('Maria Karla Espinosa','National Coordinator','mklespinosa@gmail.com','mklespinosa','".$default."'),
                 ('Roselyn Salagan','Communications Officer','fencesitter1118@gmail.com','rsalagan','".$default."'),
-                ('Joylin Saquing','Outreach Officer','joylinsaquing@gmal.com','jsaquing',".$default."'),
+                ('Joylin Saquing','Outreach Officer','joylinsaquing@gmal.com','jsaquing','".$default."'),
                 ('Johna Paula Manzano','Office Manager','','jpmanzano','".$default."'),
                 ('Leah Ivy Manzanero','Technical Specialist','','limanzanero','".$default."'),
                 ('Ryan Justin Dael','Data Visualization Specialist','','rdael','".$default."'),
