f8f715313c0d4861c76bad82acb40f564fdd9895
Thea Baraquio <raebaraquio@gmail.com>
2018-06-09 21:19:37 +0800

    Fixed production issue - activities not getting displayed for year 2015 and 2013

--- rest/functions/activities/getAll.php
+++ rest/functions/activities/getAll.php
@@ -5,7 +5,28 @@ $cond = NULL;
 if (isset($_GET['year']) && $_GET['year']!==NULL) {
 	$cond = ' where YEAR(activity_start_date) = '.mysql_escape_string(strip_tags($_GET['year']));
 } 
-
+/* Original query
+select
+	id,
+	href,
+	title,
+	activity_start_date,
+	activity_end_date,
+	activity_venue,
+	coverphoto_url,
+	writeup_content,
+	writeup_title,
+	withOfflineGallery,
+	fbPhotoAlbum_url,
+	program_url,
+	documentation_url,
+	date_added,
+	date_last_updated,
+	workshop_output_url
+from activities 
+where YEAR(activity_start_date) = '2013'
+order by date_added desc
+*/
 $query = "select
 			id,
 			href,
@@ -14,12 +35,6 @@ $query = "select
 			activity_end_date,
 			activity_venue,
 			coverphoto_url,
-			writeup_content,
-			writeup_title,
-			withOfflineGallery,
-			fbPhotoAlbum_url,
-			program_url,
-			documentation_url,
 			date_added,
 			date_last_updated,
 			workshop_output_url
@@ -31,6 +46,7 @@ $query = "select
 $getResult = mysql_query($query);
 
 if (!$getResult) {
+	// print("error" .$query);
     print(json_encode(
     	array(
     		'success'=>false,
@@ -43,17 +59,23 @@ if (!$getResult) {
     exit();
 }
 
+// print("no error" .mysql_num_rows($getResult));
+
 if (mysql_num_rows($getResult) > 0) {
-
+	// print_r(mysql_fetch_assoc($getResult));
+	// exit();
+	$activitiesData = array();
     while ($res = mysql_fetch_assoc($getResult)) {
-        $activitiesData[] = $res;
-    }
-
+		// $activitiesData[] = $res;
+		array_push($activitiesData,$res);
+	}
+	// $activitiesData = mysql_fetch_assoc($getResult);
   	print(json_encode($activitiesData));
     exit();
 }
 else {
-    print(json_encode(array()));
+	print(json_encode(array()));
+	// print("here empty array");
     exit();    
 }
 
